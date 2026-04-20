19a9c979f81d93f4c9739b46f5c6fce48361e3d3
enochakinbode <enochakinbode466@gmail.com>
2026-04-20 04:30:56 +0100

    fix: swap node for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://campusxi.app',
+  site: 'https://campusxi.com',
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
